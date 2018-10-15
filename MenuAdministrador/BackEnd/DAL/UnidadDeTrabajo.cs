4f857d119aaba8483c1a5a89086f32ee46457122
IngriidRojas <32603600+IngriidRojas@users.noreply.github.com>
2018-10-15 02:03:15 -0600

    Add files via upload

---
+++ MenuAdministrador/BackEnd/DAL/UnidadDeTrabajo.cs
@@ -0,0 +1,37 @@
+﻿using BackEnd.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd.DAL
+{
+    public class UnidadDeTrabajo<T> : IDisposable where T : class
+    {
+        private readonly SigecaEntities context;
+        //public IDALGenerico<Queja> quejaDAL;
+        //public IDALGenerico<TablaGeneral> tablaDAL;
+        public IDALGenerico<T> genericDAL;
+
+
+        public UnidadDeTrabajo(SigecaEntities _context)
+        {
+            context = _context;
+            genericDAL = new DALGenericoImpl<T>(context);
+            //    quejaDAL = new DALGenericoImpl<Queja>(context);
+            //    tablaDAL = new DALGenericoImpl<TablaGeneral>(context);
+        }
+
+        public void Complete()
+        {
+            context.SaveChanges();
+        }
+
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
